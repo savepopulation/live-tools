586948065cda0081b56be479586dea9118a1e6af
tyln <taylan.sbrcn@gmail.com>
2018-02-21 18:40:28 +0300

    fixed portrait upsidedown bug

--- live-orientation/src/main/java/com/raqun/live_orientation/DeviceOrientation.kt
+++ live-orientation/src/main/java/com/raqun/live_orientation/DeviceOrientation.kt
@@ -6,8 +6,8 @@ package com.raqun.live_orientation
 enum class DeviceOrientation(val value: Int) {
     LANDSCAPE_LEFT(1 shl 0),
     LANDSCAPE_RIGHT(1 shl 1),
-    PORTRAIT(1 shl 2),
-    UPSIDE_DOWN(1 shl 3),
+    PORTRAIT(1 shl 3),
+    UPSIDE_DOWN(1 shl 2),
     FACE_UP(1 shl 4),
     FACE_DOWN(1 shl 5);
 
