dc339bfc7bb44eec7983b09766d88914cb44aa33
Aykut Asil <aykuttasil@gmail.com>
2018-03-02 22:07:19 +0300

    live location

--- app/src/main/java/iammert/com/app/MainActivity.kt
+++ app/src/main/java/iammert/com/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.aykuttasil.livelocation.LocationLiveData
 import com.raqun.live_orientation.OrientationLiveData
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,16 @@ class MainActivity : AppCompatActivity() {
         OrientationLiveData(this)
                 .observe(this, Observer {
                     it?.let {
-                        Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
                     }
                 })
+
+        LocationLiveData(this).observe(this, Observer {
+            it?.let {
+                Toast.makeText(this, it.provider, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
     }
 }
